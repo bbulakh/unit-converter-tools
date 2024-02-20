7b2ce1d37b6b2974916ab11d2c61fb41fc9b2cfd
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-02-20 15:13:52 +0300

    set colors in tailwind config
    - set colors in talwind config
    - replace existing colors with set colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#A8EB53',
+				accent: '#EA985D',
+				typodark: '#242424',
+				typolight: '#EBEBEB'
+			  },
+		},
 	},
 	plugins: [],
 }
