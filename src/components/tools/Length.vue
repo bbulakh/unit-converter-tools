92553b62e0a71fe1125c6679539b983677389fc5
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-03-11 18:21:49 +0300

    add draft length converter

---
+++ src/components/tools/Length.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      meters: 0,
+      kilometers: 0,
+      miles: 0,
+    };
+  },
+  methods: {
+    convertToOtherUnits() {
+      this.kilometers = (this.meters / 1000).toFixed(2);
+      this.miles = (this.meters / 1609.34).toFixed(2);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <input type="number" v-model="meters" @change="convertToOtherUnits" /> Метры
+    = <input type="number" v-model="kilometers" /> Километры
+    <input type="number" v-model="miles" /> Мили
+  </div>
+</template>
