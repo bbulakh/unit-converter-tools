72c05ab1c171287ae2b26cd09945053e30d4f6cb
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-03-20 15:55:42 +0300

    add responsive for currency converter

--- src/components/tools/Converter.vue
+++ src/components/tools/Converter.vue
@@ -7,46 +7,103 @@
           type="number"
           v-model.number="amount"
       /></label>
-      <label class="mb-3">
-        From currency:
-        <select
-          class="p-2 w-full mt-2 border rounded-md h-[50px]"
-          v-model="fromCurrency"
+      <div class="md:flex items-center justify-between gap-4">
+        <label class="mb-3">
+          From currency:
+          <select
+            class="p-2 w-full mt-2 border rounded-md h-[50px]"
+            v-model="fromCurrency"
+          >
+            <option
+              v-for="(name, code) in currencies"
+              :key="code"
+              :value="code"
+            >
+              {{ code }} - {{ name }}
+            </option>
+          </select>
+        </label>
+        <button
+          class="my-3 md:mt-8 bg-accent md:rotate-90 w-10 h-10 flex items-center justify-center rounded"
+          @click.prevent="swapCurrencies"
         >
-          <option v-for="(name, code) in currencies" :key="code" :value="code">
-            {{ code }} - {{ name }}
-          </option>
-        </select>
-      </label>
-      <button class="my-3 bg-accent w-10 h-10 flex items-center justify-center rounded" @click.prevent="swapCurrencies"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-</svg>
-</button>
-      <label class="mb-3">
-        To currency:
-        <select
-          class="p-2 mt-2 w-full border rounded-md h-[50px]"
-          v-model="toCurrency"
-        >
-          <option v-for="(name, code) in currencies" :key="code" :value="code">
-            {{ code }} - {{ name }}
-          </option>
-        </select>
-      </label>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+            />
+          </svg>
+        </button>
+        <label class="mb-3">
+          To currency:
+          <select
+            class="p-2 mt-2 w-full border rounded-md h-[50px]"
+            v-model="toCurrency"
+          >
+            <option
+              v-for="(name, code) in currencies"
+              :key="code"
+              :value="code"
+            >
+              {{ code }} - {{ name }}
+            </option>
+          </select>
+        </label>
+      </div>
 
-      <div class="mb-3">
+      <div class="my-3">
         <p class="text-xl">{{ result }}</p>
       </div>
 
-      <div class="flex items-center gap-3">
-        <button class="w-full py-3 h-[50px] flex justify-center items-center text-center text-xl rounded-md bg-primary" @click.prevent="convert">Convert</button>
-  
-        
-  
+      <div class="flex items-center md:w-[200px]  gap-3">
+        <button
+          class="w-full py-3 h-[50px] flex justify-center items-center text-center text-xl rounded-md bg-primary"
+          @click.prevent="convert"
+        >
+          Convert
+        </button>
+
         <div class="">
-          <button class="relative w-[50px] h-[50px] flex items-center justify-center bg-accent rounded-md" @click.prevent="toggleDropdown"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><g fill="currentColor"><path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483m.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501m-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432z"/><path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z"/><path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5"/></g></svg></button>
-          <ul class="bg-white *:border-b mx-[2rem] border rounded *:p-2 absolute z-10 right-0 left-0" :class="{ hidden: !showDropdown }">
-            <li class="last-of-type:border-none" v-for="(conversion, index) in historyStorage" :key="index">
+          <button
+            class="relative w-[50px] h-[50px] flex items-center justify-center bg-accent rounded-md"
+            @click.prevent="toggleDropdown"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 16 16"
+            >
+              <g fill="currentColor">
+                <path
+                  d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483m.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501m-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432z"
+                />
+                <path
+                  d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0z"
+                />
+                <path
+                  d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5"
+                />
+              </g>
+            </svg>
+          </button>
+          <ul
+            class="bg-white *:border-b border max-w-[1000px] mx-auto rounded *:p-2 absolute z-10 "
+            :class="{ hidden: !showDropdown }"
+          >
+            <li
+              class="last-of-type:border-none"
+              v-for="(conversion, index) in historyStorage"
+              :key="index"
+            >
               {{ conversion.amount }} {{ conversion.fromCurrency }} =
               {{ conversion.convertedAmount }} {{ conversion.toCurrency }}
             </li>
@@ -63,7 +120,7 @@ export default {
     return {
       fromCurrency: "USD",
       toCurrency: "RUB",
-      amount: '',
+      amount: "",
       currencies: {
         AED: "United Arab Emirates Dirham",
         AFN: "Afghan Afghani",
